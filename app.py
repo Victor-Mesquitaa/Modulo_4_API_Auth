6c303c18339b14ea74e01cd6e1b2def7f3e36ae1
Luiz Victor Mesquita <luizvictor2513@gmail.com>
2026-09-15 16:37:06 -0300

    Add user creation functionality and enhance route comments

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ login_manager.init_app(app)
 login_manager.login_view = 'login' # Faz com que o Flask-Login redirecione para a rota de login quando um usuário não autenticado tenta acessar uma rota protegida.
 
 @login_manager.user_loader # Funciona como um callback que o Flask-Login usa para carregar um usuário a partir do seu ID armazenado na sessão.
-def load_user(user_id):
+def load_user(user_id): # Função que recebe o ID do usuário e retorna o objeto do usuário correspondente. O Flask-Login usa essa função para recuperar o usuário atual a partir do ID armazenado na sessão.
     return User.query.get(int(user_id))
 
 @app.route('/login', methods=['POST'])
-def login():
+def login(): # Função que lida com a autenticação do usuário. Ela recebe os dados de login (nome de usuário e senha) via JSON, verifica se o usuário existe e se a senha está correta. Se a autenticação for bem-sucedida, o usuário é logado usando a função login_user() do Flask-Login.
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
@@ -33,11 +33,24 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 @login_required # This decorator ensures that the user must be logged in to access this route. If the user is not logged in, they will be redirected to the login page.
-def logout():
+def logout(): # Função que lida com o logout do usuário. Ela é protegida pelo decorador @login_required, garantindo que apenas usuários autenticados possam acessá-la. Quando chamada, a função realiza o logout do usuário atual usando a função logout_user()
     logout_user()
     return jsonify({'message': 'Logout successful'}), 200
 
+@app.route('/user', methods=['POST'])
+@login_required # This decorator ensures that the user must be logged in to access this route. If the user is not logged in, they will be redirected to the login page.
+def create_user(): # Função que lida com a criação de um novo usuário. Ela recebe os dados do usuário (nome de usuário e senha) via JSON, verifica se o nome de usuário já existe no banco de dados e, se não existir, cria um novo usuário e o adiciona ao banco de dados. Se o nome de usuário já existir ou se os dados estiverem incompletos, a função retorna uma mensagem de erro apropriada.
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
+    if username and password:
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User created successfully'}), 201
+    else:
+        return jsonify({'message': 'Username and password are required'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
